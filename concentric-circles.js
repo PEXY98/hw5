683e15e9d8ab7ada2d8bc703390f2690ceae5805
PEXY98 <47343648+PEXY98@users.noreply.github.com>
2019-03-04 03:22:52 -0800

    Create concentric-circles.js

---
+++ concentric-circles.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+  for (var i = 10; i <= 200; i = i + 5) {
+    circle( 200, 200, i);
+    noFill()
+  }
+}
